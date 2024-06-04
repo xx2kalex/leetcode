d5901174c3d66816cf839542096f2a521eb79655
Alexander Shpakov <shpakov@wisc.edu>
2024-06-04 10:31:23 -0500

    1

---
+++ Solution.java
@@ -0,0 +1,75 @@
+class Solution
+{
+  public static boolean isAnagram(String s, String t)
+  {
+    boolean isAnagram = false;
+
+    int sLength = s.length();
+    int tLength = t.length();
+
+    // Immediate return if string lengths are not equal
+    if (sLength != tLength)
+    {
+      System.out.println("sLength != tLength");
+      return false;
+    }
+
+    System.out.println("Is every character in " + s + " also in " + t);
+    boolean foundCurChar1 = false;
+    for (int i = 0; i < sLength; i++)
+    {
+      char currChar1 = s.charAt(i);
+      System.out.println("The current char is: " + currChar1);
+      for (int j = 0; j < tLength; j++)
+      {
+        if (currChar1 == t.charAt(j))
+        {
+          System.out.println("We found the same character: " + currChar1);
+          foundCurChar1 = true;
+          continue;
+        }
+        if (j == tLength - 1 && foundCurChar1 == false)
+        {
+          System.out.println(
+              "We did not find the same character, returning immediately: " + currChar1);
+          return false;
+        }
+      }
+    }
+
+    System.out.println("Is every character in " + t + " also in " + s);
+    boolean foundCurChar2 = false;
+    for (int i = 0; i < tLength; i++)
+    {
+      char currChar2 = t.charAt(i);
+      System.out.println("The current char is: " + currChar2);
+      for (int j = 0; j < sLength; j++)
+      {
+        if (currChar2 == s.charAt(j))
+        {
+          System.out.println("We found the same character: " + currChar2);
+          foundCurChar2 = true;
+          continue;
+        }
+        if (j == tLength - 1 && foundCurChar2 == false)
+        {
+          System.out.println(
+              "We did not find the same character, returning immediately: " + currChar2);
+          isAnagram = false;
+        }
+      }
+    }
+
+    if (foundCurChar1 && foundCurChar2)
+    {
+      System.out.println("Each character that was in one string is also in the other.");
+      isAnagram = true;
+    }
+    return isAnagram;
+  }
+
+  public static void main(String[] args)
+  {
+    System.out.println(isAnagram("jar", "jam"));
+  }
+}
