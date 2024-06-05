c24dfe86194f042428307c1914b1859a53342a6a
Alexander Shpakov <shpakov@wisc.edu>
2024-06-05 09:39:02 -0500

    final solution with comments

--- 01-isAnagram/Solution.java
+++ 01-isAnagram/Solution.java
@@ -1,8 +1,15 @@
 class Solution
 {
+  /**
+   * Determines whether a string is an anagram
+   *
+   * @param s - one string
+   * @param t - another string
+   * @return - true if the string is an anagram, false otherwise
+   */
   public static boolean isAnagram(String s, String t)
   {
-    boolean isAnagram = false;
+    boolean isAnagram = true;
 
     int sLength = s.length();
     int tLength = t.length();
@@ -14,85 +21,68 @@ class Solution
       return false;
     }
 
-    System.out.println("Is every character in " + s + " also in " + t + "?");
-    boolean allCharsFound1 = false;
-    int countMatches1 = 0;
-    int numMatches1 = 0;
+    // Checking if every character in one string also exists in the other
     for (int i = 0; i < sLength; i++)
     {
       char currChar1 = s.charAt(i);
-      System.out.println("The current char is: " + currChar1);
       boolean foundCurChar1 = false;
-      numMatches1 = s.length() - s.replaceAll(Character.toString(currChar1), "").length();
+
       for (int j = 0; j < tLength; j++)
       {
         if (currChar1 == t.charAt(j))
         {
-          countMatches1++;
-          System.out.println("We found the same character: " + currChar1);
           foundCurChar1 = true;
-          continue;
         }
         if (j == tLength - 1 && foundCurChar1 == false)
         {
-          System.out.println(
-              "We did not find the same character, returning immediately: " + currChar1);
-          return false;
-        }
-        if ((j == tLength-1) && (countMatches1 != numMatches1))
-        {
-          System.out.println(
-              "Between the two strings, there exist characters which appear different amounts of " +
-                  "times.");
           return false;
         }
       }
 
-      if (i == sLength - 1 && foundCurChar1 == true)
+      // Checking that a particular character appears in one string the same amount of times as
+      // in the other
+      int amtOfCharInS = amtOfAChar(s, currChar1);
+      int amtOfCharInT = amtOfAChar(t, currChar1);
+      if (amtOfCharInS != amtOfCharInT)
       {
-        allCharsFound1 = true;
+        return false;
       }
     }
 
-    System.out.println("Is every character in " + t + " also in " + s + "?");
-    boolean allCharsFound2 = false;
-    for (int i = 0; i < tLength; i++)
-    {
-      char currChar2 = t.charAt(i);
-      System.out.println("The current char is: " + currChar2);
-      boolean foundCurChar2 = false;
-      for (int j = 0; j < sLength; j++)
-      {
-        if (currChar2 == s.charAt(j))
-        {
-          System.out.println("We found the same character: " + currChar2);
-          foundCurChar2 = true;
-          continue;
-        }
-        if (j == tLength - 1 && foundCurChar2 == false)
-        {
-          System.out.println(
-              "We did not find the same character, returning immediately: " + currChar2);
-          isAnagram = false;
-        }
-      }
-
-      if (i == tLength - 1 && foundCurChar2 == true)
-      {
-        allCharsFound2 = true;
-      }
-    }
-    if (allCharsFound1 && allCharsFound2)
-    {
-      System.out.println("Each character that was in one string is also in the other.");
-      isAnagram = true;
-    }
     return isAnagram;
   }
 
+  /**
+   * Helper method to determine the amount of occurences of a specific character in a given string
+   *
+   * @param s - the string
+   * @param c - the particular character
+   * @return
+   */
+  private static int amtOfAChar(String s, char c)
+  {
+    int numMatches = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+      char currChar = s.charAt(i);
 
+      if (currChar == c)
+      {
+        numMatches++;
+      }
+    }
+
+    return numMatches;
+  }
+
+  /**
+   * Main method for testing
+   *
+   * @param args
+   */
   public static void main(String[] args)
   {
-    System.out.println(isAnagram("racecar", "carrace"));
+    System.out.println(isAnagram("bbcc", "ccbc")); // Expected: False
+    System.out.println(isAnagram("racecar", "carrace")); // Expected True
   }
 }
