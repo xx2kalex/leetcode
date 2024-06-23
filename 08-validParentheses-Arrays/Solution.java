6bd99286410a1992d652deb9c9e231bb150f4c27
Alexander Shpakov <shpakov@wisc.edu>
2024-06-23 14:00:02 -0500

    2 - problem solved

--- 08-validParentheses-Arrays/Solution.java
+++ 08-validParentheses-Arrays/Solution.java
@@ -1,80 +1,74 @@
+/**
+ * NeetCode 150
+ * "Valid Parentheses"
+ * 2024-06-23
+ */
+
 import java.util.ArrayList;
 
 class Solution
 {
   public static boolean isValid(String s)
   {
-    ArrayList<Character> stack = new ArrayList<Character>();
+    ArrayList<Character> stack = new ArrayList<>();
+    char c;
+
+    if (s.length() % 2 != 0)
+    {
+      // String cannot have an odd number of characters
+      return false;
+    }
+
 
     // Adds all the characters in the String to a stack
     for (int i = 0; i < s.length(); i++)
     {
-      stack.add(s.charAt(i));
-    }
-
-    // Checks if all parentheses have an inverse
-    if (!containsInverse(stack))
-    {
-      return false;
-    }
-
-    char currChar;
-    int currIndex;
-
-    return true;
-  }
-
-  private static boolean containsInverse(ArrayList<Character> stack)
-  {
-    if ((stack.contains('(') && !stack.contains(')')) ||
-        (stack.contains(')') && !stack.contains('(')))
-    {
-      return false;
-    }
-    if ((stack.contains('[') && !stack.contains(']')) ||
-        (stack.contains(']') && !stack.contains('[')))
-    {
-      return false;
-    }
-    if ((stack.contains('{') && !stack.contains('}')) ||
-        (stack.contains('}') && !stack.contains('{')))
-    {
-      return false;
-    }
-
-    return true;
-  }
-
-
-  private static boolean correctOrder(String s)
-  {
-    boolean openingParenthese;
-
-    for (int i = 0; i < s.length(); i++)
-    {
-      // Determine if the current parenthese is an opening one or closing one
-      if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+      if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
       {
-        openingParenthese = true;
+        // Add the opening bracket to the top of the stack and move on to next char
+        stack.add(s.charAt(i));
+        continue;
+      }
+      if (s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}')
+      {
+        // First character cannot be a closing bracket
+        return false;
       }
       else
       {
-        openingParenthese = false;
-      }
-
-      if (openingParenthese)
-      {
-        for (int j = i+1; i < s.length(); i++)
+        if (stack.isEmpty())
         {
-          if (!openingParenthese || !(s.charAt(j) == //the exact inverse))
+          // This means that there are no opening brackets to compare with
+          return false;
+        }
+        c = stack.remove(stack.size() - 1);
+        if (c == '(' && s.charAt(i) != ')')
+        {
+          return false;
+        }
+        else if (c == '[' && s.charAt(i) != ']')
+        {
+          return false;
+        }
+        else if (c == '{' && s.charAt(i) != '}')
+        {
+          return false;
         }
       }
     }
+
+    if (!stack.isEmpty())
+    {
+      // IF we have successful orderings, the stack should be empty by the end of the iterations
+      return false;
+    }
+
+    return true;
   }
 
   public static void main(String[] args)
   {
-    String s = "(][)";
+    String s = "((";
     System.out.println(isValid(s));
   }
 }
